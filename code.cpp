aa0c8a187802e5b04227c39227a3585895c5930a
Yonathan Nieles <yonathannieles011@gmail.com>
2026-08-01 20:56:17 -0400

    Definicion de pines de los semaforos vehiculares

---
+++ code.cpp
@@ -0,0 +1,8 @@
+// --- PINES SEMÁFOROS VEHICULARES ---
+const int LED_V_NS = 13; // Verde Vehicular Norte-Sur
+const int LED_A_NS = 12; // Amarillo Vehicular Norte-Sur
+const int LED_R_NS = 11; // Rojo Vehicular Norte-Sur
+
+const int LED_V_EO = 8;  // Verde Vehicular Este-Oeste
+const int LED_A_EO = 7;  // Amarillo Vehicular Este-Oeste
+const int LED_R_EO = 6;  // Rojo Vehicular Este-Oeste
